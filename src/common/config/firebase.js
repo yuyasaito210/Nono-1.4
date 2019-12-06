56d4905d24d6ce357a049fbde534ce856b36cdf4
yuyasaito210 <yuyasaito210@gmail.com>
2019-12-06 12:30:01 -0800

    migrated firebase

--- src/common/config/firebase.js
+++ src/common/config/firebase.js
@@ -1,4 +1,5 @@
 // com.laplus.nono
+/*
 export default firebaseConfig = {
   ios: {
     clientId: '3300133003-g3cnaab5gdhnce4sai73j7mfi0qf1avi.apps.googleusercontent.com',
@@ -33,18 +34,39 @@ export default firebaseConfig = {
     measurementId: "G-YFE6BWR3FZ"
   }  
 }
-
-// com.chargeme.nono
-// Will change this later
-/*
-export default firebaseConfig = {
-  apiKey: "AIzaSyBBx0QJx7WaFNYgjG84uBAr9HAl9Js3vD0",
-  authDomain: "nono-app-a7dde.firebaseapp.com",
-  databaseURL: "https://nono-app-a7dde.firebaseio.com",
-  projectId: "nono-app-a7dde",
-  storageBucket: "nono-app-a7dde.appspot.com",
-  messagingSenderId: "725402495129",
-  appId: "1:725402495129:web:f97c21532bef343b45f008",
-  measurementId: "G-NHC57DHN37"
-};
 */
+// com.chargeme.nono
+export default firebaseConfig = {
+  ios: {
+    clientId: '725402495129-6pccjqd3lt4u1ohp4u5ji5lmiliitvuk.apps.googleusercontent.com',
+    appId: '1:725402495129:ios:16e555fcd82c74b745f008',
+    apiKey: 'AIzaSyBwINjstRC_Slxjj8s1HnS7k__4y5J3OQ8',
+    databaseURL: 'https://nono-app-a7dde.firebaseio.com',
+    storageBucket: 'nono-app-a7dde.appspot.com',
+    messagingSenderId: '3300133003',
+    projectId: '"nono-81379',
+    // persistence: true,
+  },
+  android: {
+    clientId: '3300133003-g3cnaab5gdhnce4sai73j7mfi0qf1avi.apps.googleusercontent.com',
+    apiKey: "AIzaSyDxlkgWVpxz-WD3ZJwIKLKkUZ47KSJ7cDM",
+    authDomain: "nono-81379.firebaseapp.com",
+    databaseURL: "https://nono-81379.firebaseio.com",
+    projectId: "nono-81379",
+    storageBucket: "nono-app-a7dde.appspot.com",
+    messagingSenderId: "3300133003",
+    appId: "1:3300133003:web:c092e967904ac428a6ec8e",
+    measurementId: "G-YFE6BWR3FZ",
+    // persistence: true,
+  },
+  web: {
+    apiKey: "AIzaSyBBx0QJx7WaFNYgjG84uBAr9HAl9Js3vD0",
+    authDomain: "nono-app-a7dde.firebaseapp.com",
+    databaseURL: "https://nono-app-a7dde.firebaseio.com",
+    projectId: "nono-app-a7dde",
+    storageBucket: "nono-app-a7dde.appspot.com",
+    messagingSenderId: "725402495129",
+    appId: "1:725402495129:web:f97c21532bef343b45f008",
+    measurementId: "G-NHC57DHN37"
+  }
+};
